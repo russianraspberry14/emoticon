963c47c5f507e836fd1b6f8caa73b4d45b80b3c1
ekanshsahu <sahuekansh11@gmail.com>
2025-01-25 21:40:22 -0500

    Changes to the web app's appointments

--- knowtion/src/Components/Navbar.jsx
+++ knowtion/src/Components/Navbar.jsx
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import image from '../assets/1.png';
 import './navbar.css';
 
 const Navbar = () => {
+    const [showNavbar, setShowNavbar] = useState(true);
+    const [lastScrollY, setLastScrollY] = useState(0);
+
+    const handleScroll = () => {
+        const currentScrollY = window.scrollY;
+
+        if (currentScrollY > lastScrollY && currentScrollY > 50) {
+            // Scrolling down, hide the navbar
+            setShowNavbar(false);
+        } else {
+            // Scrolling up, show the navbar
+            setShowNavbar(true);
+        }
+
+        setLastScrollY(currentScrollY);
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [lastScrollY]);
+
     return (
-        <nav className="navbar">
+        <nav className={`navbar ${showNavbar ? 'visible' : 'hidden'}`}>
             <div className="brand">
-                <img src={image} alt="Knowtion" />
+                <img src={image} alt="Knowtion" className="navbar-image" />
             </div>
-            <ul className="links">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Link 2</a></li>
-                <li><a href="#">Link 3</a></li>
-                <li className="dropdown">
-                    <a href="#">Dropdown</a>
-                    <div className="dropdown-content">
-                        <a href="#">Dropdown Item 1</a>
-                        <a href="#">Dropdown Item 2</a>
-                        <a href="#">Dropdown Item 3</a>
-                    </div>
-                </li>
-            </ul>
         </nav>
     );
 };
