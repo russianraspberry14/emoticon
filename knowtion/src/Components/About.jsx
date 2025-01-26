8a405ea625dfcd6e9fa7f2aca290bee971cdadd9
rudrank1 <rudrank2004@gmail.com>
2025-01-26 07:22:48 -0500

    Updated Webpage About Section

--- knowtion/src/Components/About.jsx
+++ knowtion/src/Components/About.jsx
@@ -3,9 +3,25 @@ import React, { useState } from "react";
 const About = () => {
     // Array of card data
     const cards = [
-        { id: 1, title: "Heart Rate", description: "We utilize your heartbeat and motion to analyze your stressful moments to keep track when you can't!" },
-        { id: 2, title: "Voice Analysis", description: "Detect your tone while having conversations to analyze for any mood swings or extreme moods" },
-        { id: 3, title: "Comprehensive Reports", description: "Generate comprehensive reports" },
+        {
+            id: 1, title: "Heart Rate", description: "We are in sync with your heartbeat."
+                + "\nOur heart tracker will help you record aberrations your heartbeat to analyze your stressful moments and keep track when you can't!"
+        },
+        {
+            id: 2, title: "Voice Analysis", description: "Our state-of-the-art emotion detection software is keeping an ear out for you."
+                + "\nWe listen to your daily interactions and note sudden changes in tone, so that you keep track and introspect on your day later."
+        },
+        {
+            id: 3, title: "Comprehensive Reports", description: "From the data you provide us, we generate reports."
+                + "\nWe log incidents that stand out, along with their timings."
+                + "\nWe help you conceptualize just how much your interactions affect your mental state over the course of a typical day."
+        },
+        {
+            id: 4, title: "Medical Collaboration", description: "Manage and track your medical appointments."
+                + "\nNever miss an appointment again, as we start reminding you two days in advance."
+                + "\nYour approved doctors can view your in-app reports."
+                + "\nEase of communication between different healthcare providers through our app reduces medical miscommunication."
+        },
     ];
 
     // State to track the current card index
